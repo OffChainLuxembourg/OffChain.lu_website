48417724f0710055f663c4e0d512ad61747aad2a
Julian Gonzalez <joticajulian@gmail.com>
2024-12-05 18:25:35 +0100

    default NextJS project

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('#eventsCarousel img').forEach(item => {
-    item.addEventListener('click', function () {
-        const modalImage = document.getElementById('modalImage');
-        modalImage.src = this.src; // Set the source of the modal image to the source of the clicked image
-        const modalTitle = document.getElementById('imageModalLabel');
-        modalTitle.textContent = this.alt; // Optionally set the modal title to the alt text of the image
-    });
-});
