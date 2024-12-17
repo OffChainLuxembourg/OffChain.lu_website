6bba8dcf2d380a1370e1b702cf4db0669fdec2db
Julian Gonzalez <joticajulian@gmail.com>
2024-12-17 10:06:05 +0100

    whitepaper link in footer

--- data/footer.js
+++ data/footer.js
@@ -1,4 +1,5 @@
 export const footerLinks = [
+  { href: "https://offchain-luxembourg.gitbook.io/whitepaper", text: "Whitepaper" },
   { href: "/about", text: "Offchain About" },
   { href: "/service-1", text: "Our project" },
   { href: "/project", text: "Join us" },
